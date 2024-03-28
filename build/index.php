5350146d513ed82bf540ff928a7007c6562535bb
divinechukwuma <divinechukwuma982@gmail.com>
2024-03-28 19:48:29 +0100

    im tired

---
+++ build/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+</head>
+<body>
+
+<?php
+
+// $cool = "hey";
+
+// echo "$cool wassup";
+
+// $name = "Dark matter";
+
+// $read = false;
+
+// //When using boolean the default is true so if u just put the naame of a variable it  is automatically true
+
+// if ($read){
+    
+//     $message = "you have read $name";
+
+// }else{
+
+//     $message = "you have NOT read $name";
+
+// }
+
+ 
+
+// ?>
+
+// <h1>
+//     <?= $message ?>
+// </h1>
+
+<h1> Recommended book</h1>
+
+<?php 
+
+ $books = ["Rich dad Poor dad",
+            "hail mary",
+            "How to talk to anyone"
+ ]
+
+?>
+
+<ul>
+    <li>Poor dad Rich dad</li>
+    <li>hail mary</li>
+    <li>How to talk to anyone</li>
+</ul>
+    
+</body>
+</html>
