039578adbedc6d6dbb651a7b1090419639251f61
divinechukwuma <divinechukwuma982@gmail.com>
2024-04-02 12:21:14 +0100

    still stucked

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ require 'functions.php';
 
 $uri = $_SERVER['REQUEST_URI'];
 
- if ($uri === "/index.php/") {
+ if ($uri === "controller/index.php") {
      require 'controller/index.php';
- } 
+ } elseif($uri === '/controller/about.php'){
+    require 'controller/about.php';
+ }
