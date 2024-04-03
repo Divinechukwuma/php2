7869925cfda2632e1fd500be2c14c50256cd4b50
divinechukwuma <divinechukwuma982@gmail.com>
2024-04-03 10:37:26 +0100

    im working

--- index.php
+++ index.php
@@ -1,6 +1,33 @@
-<?php 
-
-// require '.htaccess';
+<?php
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+class person{
+    
+    public $name;
+    public $age;
+
+    public function breathe(){
+
+        echo $this->name . ' is breathing! and he is ' . $this->age;
+
+    }
+
+}
+
+$person = new person();
+
+$person->name = 'john doe';
+$person->age = 25;
+
+$person->breathe();
+
+// $person2 = new person();
+
+// $person2->name = 'divine';
+// $person2->age = 30;
+
+// dd($person);
+
+
