d244a770ff5880b3082871c0cbb1da896b97b166
divinechukwuma <divinechukwuma982@gmail.com>
2024-04-09 10:09:09 +0100

    im awesome

--- router.php
+++ router.php
@@ -1,25 +1,8 @@
 <?php
 
-$routes = require ("routes.php");
+var_dump('hello there');
+die();
 
-//this is a refractor of the original code its an array of the uri then it checked the called and requireds the $uri
-
-// set the 404 as the default  you can change it in the function
-
-
-
-
-   function abort($code = 404)
-   {
-
-      http_response_code($code);
-
-      require "views/{$code}.php";
-
-      die();
-   }
-
-   
 function routeToController($uri, $routes)
 {
 
@@ -30,7 +13,19 @@ function routeToController($uri, $routes)
       //if u dont input anything it will send the 404 message but u can change the default
       abort();
    }
-} 
+}
+
+$routes = require("routes.php");
+
+//this is a refractor of the original code its an array of the uri then it checked the called and requireds the $uri
+// set the 404 as the default  you can change it in the function
+function abort($code = 404)
+{
+   http_response_code($code);
+   require "views/{$code}.php";
+   die();
+}
+
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
