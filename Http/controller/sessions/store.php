d6ed4c849c1c65523e5ef4b02e7519a20c581237
divinechukwuma <divinechukwuma982@gmail.com>
2024-04-30 03:24:43 +0100

    i am done refactoring my login in page code

--- Http/controller/sessions/store.php
+++ Http/controller/sessions/store.php
@@ -1,51 +1,26 @@
 <?php
 
-//login the user if the credential matches
-
-use core\Database;
-use core\validator;
-use core\App;
+use core\Authenticator;
 use Http\Forms\LoginForm;
 
-$db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password  = $_POST['password'];
 
- $form = new LoginForm();
+$form = new LoginForm();
 
- if (! $form->validate($email,$password)){
+if ($form->validate($email, $password)) {
 
-    if (!empty($errors)) {
-        return view('sessions/create.view.php', [
-            'errors' => $form->errors()
-        ]);
+    $auth = new Authenticator();
+
+    if ($auth->attempt($email, $password)) {
+
+        redirect('location: /webapps/php2/about');
     }
 
+    $form->error('email', 'No matching account for that email address and password.');
 };
 
-
-//match the credentials
-$user = $db->query('SELECT * FROM user WHERE email = :email', [
-    'email' => $email
-])->find();
-
-if ($user) {
-
-    //we have a user buy we have the check if the the password matches
-    if (password_verify($password, $user['password'])) {
-        login([
-            'email' => $email
-        ]);
-
-        header('location: /webapps/php2/about');
-        exit();
-    }
-}
-
-
 return view('sessions/create.view.php', [
-    'errors' => [
-        'email' => 'No matching account for that email address and password.'
-    ]
+    'errors' => $form->errors()
 ]);
---
+++ core/Authenticator.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace core;
+
+class Authenticator
+{
+    public function attempt($email, $password)
+    {
+
+        // match the credentials
+        $user = App::resolve(Database::class)->query('SELECT * FROM user WHERE email = :email', [
+            'email' => $email
+        ])->find();
+
+        if ($user) {
+
+            //we have a user buy we have the check if the the password matches
+            if (password_verify($password, $user['password'])) {
+                $this->login([
+                    'email' => $email
+                ]);
+
+                return true;
+
+            }
+        }
+
+        return false;
+        
+    }
+
+
+    public  function login($user)
+    {
+        $_SESSION['user'] = [
+            'email' => $user['email'],
+        ];
+
+        session_regenerate_id(true);
+    }
+
+    public function logout()
+    {
+        //Log out the user
+
+        $_SESSION = [];
+
+        session_destroy();
+
+        $params = session_get_cookie_params();
+        setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+    }
+}
